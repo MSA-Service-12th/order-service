3351fa3b5d29e902eee026c8992dbc44e6c85e73
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-02 00:12:57 +0900

    feat: 주문 상태 변경 오류 관련 에러코드 추가

--- src/main/java/com/loopang/orderservice/domain/exception/OrderErrorCode.java
+++ src/main/java/com/loopang/orderservice/domain/exception/OrderErrorCode.java
@@ -8,6 +8,7 @@ public enum OrderErrorCode implements ErrorCodeSpec {
 	ORDER_NOT_FOUND("ORDER_001", HttpStatus.NOT_FOUND, "주문 정보를 찾을 수 없습니다."),
 	ORDER_INSUFFICIENT_STOCK("ORDER_002", HttpStatus.BAD_REQUEST, "주문한 상품의 재고가 부족합니다."),
 	ORDER_ACCESS_DENIED("ORDER_OO3", HttpStatus.FORBIDDEN, "해당 주문에 관한 권한이 없습니다."),
+	ORDER_INVALID_STATUS_TRANSITION("ORDER_004", HttpStatus.BAD_REQUEST, "주문상태를 변경할 수 없습니다.")
 
 	;
 
