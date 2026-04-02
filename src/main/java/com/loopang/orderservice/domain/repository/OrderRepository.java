ac3e51e50f30f17e514c0134b94ed75984f4a995
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-02 11:22:32 +0900

    docs: README.md 파일 오탈자 수정

--- src/main/java/com/loopang/orderservice/domain/repository/OrderRepository.java
+++ src/main/java/com/loopang/orderservice/domain/repository/OrderRepository.java
@@ -1,9 +1,14 @@
-package com.loopang.orderservice.domain.repository;
+package com.loopang.orderservice.domain.query;
 
 import com.loopang.orderservice.domain.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+import java.util.UUID;
 
 public interface OrderRepository {
 
-	Order save(Order order);
+	Optional<Order> findById(UUID orderId);
+	Page<Order> findAll(Pageable pageable);
 }
