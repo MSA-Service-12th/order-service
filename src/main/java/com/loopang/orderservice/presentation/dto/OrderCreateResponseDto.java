a71ebc77d9302abbd4fa13401f45829fa2c3e57f
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-02 14:52:53 +0900

    feat: 주문 presentation 계층 컨트롤러, dto 추가

---
+++ src/main/java/com/loopang/orderservice/presentation/dto/OrderCreateResponseDto.java
@@ -0,0 +1,14 @@
+package com.loopang.orderservice.presentation.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderCreateResponseDto {
+
+
+}
