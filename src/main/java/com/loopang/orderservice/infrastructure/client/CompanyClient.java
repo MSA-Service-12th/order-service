c97dcdf9b8ecf3e727df19a369e633c2c11c4418
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-02 14:37:09 +0900

    feat: ãFeignClient 관련 연동용 인터페이스 추가

---
+++ src/main/java/com/loopang/orderservice/infrastructure/client/CompanyClient.java
@@ -0,0 +1,7 @@
+package com.loopang.orderservice.infrastructure.client;
+
+// FeignClient 사용: 지정한 업체ID에 해당하는 공급업체, 수령업체의 상세정보 조회
+public interface CompanyClient {
+
+	// TODO: FeignClient 연동
+}
---
+++ src/main/java/com/loopang/orderservice/infrastructure/client/HubClient.java
@@ -0,0 +1,7 @@
+package com.loopang.orderservice.infrastructure.client;
+
+// feignClient(허브정보 조회 - 지정한 허브ID의 허브명, 허브주소)
+public interface HubClient {
+
+	// TODO: FeignClient 연동
+}
