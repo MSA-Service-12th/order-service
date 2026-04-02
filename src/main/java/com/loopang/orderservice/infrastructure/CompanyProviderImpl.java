074110feabc068967673fb2b31532427e35f9179
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-02 16:16:15 +0900

    chore: CompanyProviderImpl 미구현 사항 관련 예외 처리 문구 수정

--- src/main/java/com/loopang/orderservice/infrastructure/CompanyProviderImpl.java
+++ src/main/java/com/loopang/orderservice/infrastructure/CompanyProviderImpl.java
@@ -1,9 +1,7 @@
 package com.loopang.orderservice.infrastructure;
 
 import com.loopang.orderservice.domain.service.CompanyProvider;
-import com.loopang.orderservice.domain.service.HubProvider;
 import com.loopang.orderservice.domain.service.UserProvider;
-import com.loopang.orderservice.domain.vo.HubInfo;
 import com.loopang.orderservice.domain.vo.Receiver;
 import com.loopang.orderservice.domain.vo.Supplier;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +17,12 @@ public class CompanyProviderImpl implements CompanyProvider {
 
 	@Override
 	public Supplier getSupplier(UUID supplierId, String requirements) {
-		throw new UnsupportedOperationException("ItemClient Feign 연동이 필요합니다");
+		throw new UnsupportedOperationException("CompanyClient Feign 연동이 필요합니다");
 	}
 
 	// TODO: 수령업체 담당자에 관한 추가정보 조회 시 사용자 도메인 조회
 	@Override
 	public Receiver getReceiver(UUID receiverId, UserProvider userProvider) {
-		throw new UnsupportedOperationException("ItemClient Feign 연동이 필요합니다");
+		throw new UnsupportedOperationException("CompanyClient Feign 연동이 필요합니다");
 	}
 }
