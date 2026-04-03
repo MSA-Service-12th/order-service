a2d0567abc56a6caf8070678921996357c5652f4
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-03 10:42:06 +0900

    feat: 주문 생성, 조회, 검색, 삭제용 컨트롤러 메서드 추가

--- src/main/java/com/loopang/orderservice/presentation/OrderController.java
+++ src/main/java/com/loopang/orderservice/presentation/OrderController.java
@@ -1,19 +1,12 @@
 package com.loopang.orderservice.presentation;
 
-import com.loopang.orderservice.application.dto.OrderCreateCommandDto;
-import com.loopang.orderservice.application.dto.OrderDeleteCommandDto;
-import com.loopang.orderservice.application.dto.OrderDetailsDto;
-import com.loopang.orderservice.application.dto.OrderSearchConditionDto;
+import com.loopang.orderservice.application.dto.*;
 import com.loopang.orderservice.application.service.OrderCommandService;
 import com.loopang.orderservice.application.service.OrderQueryService;
 import com.loopang.orderservice.domain.vo.UserType;
-import com.loopang.orderservice.presentation.dto.OrderCreateRequestDto;
-import com.loopang.orderservice.presentation.dto.OrderDeleteResponseDto;
-import com.loopang.orderservice.presentation.dto.OrderListResponseDto;
-import com.loopang.orderservice.presentation.dto.OrderResponseDto;
+import com.loopang.orderservice.presentation.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -30,40 +23,31 @@ public class OrderController {
 	private final OrderQueryService orderQueryService;
 
 	@PostMapping
-	public OrderResponseDto createOrder(@RequestBody OrderCreateRequestDto requestDto) {
-		OrderDetailsDto details = orderCommandService.createOrder(OrderCreateCommandDto.from(requestDto));
-		return OrderResponseDto.from(details);
+	public OrderCreateResponseDto createOrder(@RequestBody OrderCreateRequestDto requestDto) {
+		OrderCreateResultDto result = orderCommandService.createOrder(OrderCreateCommandDto.from(requestDto));
+
+		return OrderCreateResponseDto.from(result);
 	}
 
 	@GetMapping("/{orderId}")
-	public OrderResponseDto getOrder(
+	public OrderDetailResponseDto getOrder(
 			@PathVariable UUID orderId,
 			@RequestHeader(value = "X-User-Id") UUID userId,
 			@RequestHeader(value = "X-User-Role") String userRole) {
 		OrderDetailsDto details = orderQueryService.getOrder(orderId, userId, UserType.from(userRole));
-		return OrderResponseDto.from(details);
+
+		return OrderDetailResponseDto.from(details);
 	}
 
 	@GetMapping
-	public Page<OrderListResponseDto> searchOrders(
+	public Page<OrderSummaryResponseDto> searchOrders(
 			@ModelAttribute OrderSearchConditionDto condition,
 			@PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
 			@RequestHeader(value = "X-User-Id") UUID userId,
 			@RequestHeader(value = "X-User-Role") String userRole) {
 
-		int size = pageable.getPageSize();
-		if (size != 10 && size != 30 && size != 50) {
-			size = 10;
-		}
-
-		Pageable adjustedPageable = PageRequest.of(
-				pageable.getPageNumber(),
-				size,
-				pageable.getSort()
-		);
-
-		return orderQueryService.searchOrders(condition, adjustedPageable, userId, UserType.from(userRole))
-				.map(OrderListResponseDto::from);
+		return orderQueryService.searchOrders(condition, pageable, userId, UserType.from(userRole))
+				.map(OrderSummaryResponseDto::from);
 	}
 
 	@DeleteMapping("/{orderId}")
@@ -71,8 +55,8 @@ public class OrderController {
 			@PathVariable UUID orderId,
 			@RequestHeader(value = "X-User-Id") UUID userId,
 			@RequestHeader(value = "X-User-Role") String userRole) {
-		OrderDeleteCommandDto result
-				= orderCommandService.deleteOrder(OrderDeleteCommandDto.of(orderId, userId, UserType.from(userRole)));
+		OrderDeleteCommandDto result = orderCommandService.deleteOrder(orderId, userId);
+
 		return OrderDeleteResponseDto.from(result);
 	}
 }
