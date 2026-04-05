ed84e7cf17e480c23cbc11c50c595a6935697b38
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-04-05 20:21:35 +0900

    test: 이벤트 처리 관련 테스트코드 추가

--- src/main/java/com/loopang/orderservice/OrderserviceApplication.java
+++ src/main/java/com/loopang/orderservice/OrderserviceApplication.java
@@ -3,11 +3,9 @@ package com.loopang.orderservice;
 import com.loopang.common.config.JpaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
 @Import(JpaConfig.class)
-@EnableFeignClients // 공통모듈에 어노테이션 추가될 시 삭제 예정
 @SpringBootApplication
 public class OrderserviceApplication {
 
